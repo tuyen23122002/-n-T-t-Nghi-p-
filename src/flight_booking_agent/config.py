c9e5f3fe7a859a01e0f9730517ffa5f3850835a8
Tuyen <you@example.com>
2025-10-06 03:58:21 +0000

    add code

--- src/flight_booking_agent/config.py
+++ src/flight_booking_agent/config.py
@@ -1,10 +1,13 @@
 # src/flight_booking_agent/config.py
 import os
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
+# Thay đổi import
+from langchain_google_vertexai import ChatVertexAI
 from .tools.flight_tools import search_flights_tool
 
+
 load_dotenv()
+""" 
 VLLM_BASE_URL = os.getenv("VLLM_BASE_URL", "http://localhost:8000/v1")
 
 # LLM thông thường để trò chuyện
@@ -13,6 +16,14 @@ llm = ChatOpenAI(
     openai_api_key="EMPTY",
     openai_api_base=VLLM_BASE_URL,
     temperature=0.7
+)
+
+"""
+llm = ChatVertexAI(
+    model_name="gemini-2.5-pro", # Tên model vẫn giữ nguyên
+    temperature=0.7,
+    project="end-to-end-agentic-rag",  # Thay bằng ID dự án GCP của bạn
+    location="us-central1",      # Ví dụ: "us-central1"
 )
 
 # LLM đặc biệt có khả năng sử dụng tools
